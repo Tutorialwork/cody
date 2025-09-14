c06df57a4d48dac84acd7840333c42736e73e0e4
Tutorialwork <tutorialworktv@gmail.com>
2025-09-14 17:15:57 +0200

    added firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:cody/services/get_it_service.dart';
 import 'package:cody/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'constants/list_constants.dart';
+import 'firebase_options.dart';
 import 'l10n/app_localizations.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   GetItService.registerSingletons();
 
   runApp(MaterialApp(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import firebase_core
 import flutter_cloud_kit
 import flutter_secure_storage_macos
 import local_auth_darwin
@@ -17,6 +18,7 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterCloudKitPlugin.register(with: registry.registrar(forPlugin: "FlutterCloudKitPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
